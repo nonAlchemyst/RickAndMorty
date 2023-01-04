eba1fb8df9a1af6260fd93f3785a417fd6a350e2
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-01-04 22:36:28 +0500

    Add BottomNavigationView
    Add CharactersFragment.kt as first fragment

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() = with(binding) {
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.characters_menu_item, R.id.locations_menu_item, R.id.episodes_menu_item
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navigation.setupWithNavController(navController)
     }
 }
